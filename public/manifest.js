761b836a73af270e62f006ba0a7113cf713cb56d
Muswab320 <rn5y7tzbtb@privaterelay.appleid.com>
2026-08-24 18:11:49 +0300

    Delete public/manifest.js

--- public/manifest.js
+++
@@ -1,9 +0,0 @@
-{
-  "name": "Sharp Entry V3",
-  "short_name": "Sharp Entry",
-  "description": "XAU/USD Sharp Entry Scanner",
-  "start_url": "/",
-  "display": "standalone",
-  "background_color": "#090909",
-  "theme_color": "#090909"
-}
